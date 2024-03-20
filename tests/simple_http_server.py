9eb60f8bc35044e0c5369457bc3aaea905df90e3
Timo Weiss <timo.weiss@protonmail.ch>
2024-03-20 11:19:58 +0100

    Advanced mock api server

--- tests/simple_http_server.py
+++ tests/simple_http_server.py
@@ -1,15 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
+httpd = None
 
-    def __init__(self, endpoints, *args, **kwargs):
-        self.endpoints = endpoints
-        super().__init__(*args, **kwargs)
+class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
+    def get_endpoints(self):
+        return []
 
     def do_GET(self):
         path = self.path
-        if path in self.endpoints:
+        endpoints = self.get_endpoints()
+        if path in endpoints:
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
@@ -22,13 +23,36 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
             response = {"message": "Not Found"}
             self.wfile.write(json.dumps(response).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=CustomHTTPRequestHandler, addr='', port=8000, endpoints=[]):
-    def handler(*args, **kwargs):
-        return handler_class(endpoints, *args, **kwargs)
+def handler_factory(endpoints):
+    """
+    Factory function to create a handler class with a customized get_endpoints method.
+    """
+    def get_endpoints():
+        return endpoints
 
+    # Return a new type dynamically creating a subclass of CustomHTTPRequestHandler
+    return type(
+        'CustomizedHandler',
+        (CustomHTTPRequestHandler,),
+        {'get_endpoints': staticmethod(get_endpoints)}
+    )
+
+def run(addr='', port=8000, endpoints=[]):
+    global httpd
+    handler_class = handler_factory(endpoints)
     server_address = (addr, port)
-    httpd = server_class(server_address, handler)
+    httpd = HTTPServer(server_address, handler_class)
+    print(f"Starting server on {port}...")
     httpd.serve_forever()
 
+def stop():
+    global httpd
+    if httpd:
+        httpd.shutdown()
+
 if __name__ == '__main__':
-    run(endpoints=['/api', '/v9/dashboard'])
+    try:
+        run(endpoints=['/api', '/v9/dashboard'])
+    except KeyboardInterrupt:
+        stop()
+        print("\nServer stopped.")
